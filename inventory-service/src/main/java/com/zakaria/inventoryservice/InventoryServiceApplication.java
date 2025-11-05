9dd5f4a664d0f210e217bac668965acf2074f906
= <=>
2025-11-05 21:30:43 +0100

    added repo instead of the folder path in configuration

--- inventory-service/src/main/java/com/zakaria/inventoryservice/InventoryServiceApplication.java
+++ inventory-service/src/main/java/com/zakaria/inventoryservice/InventoryServiceApplication.java
@@ -5,11 +5,13 @@ import com.zakaria.inventoryservice.repository.ProductRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.context.annotation.Bean;
 
 import java.util.UUID;
 
 @SpringBootApplication
+//@RefreshScope
 public class InventoryServiceApplication {
 
     public static void main(String[] args) {
